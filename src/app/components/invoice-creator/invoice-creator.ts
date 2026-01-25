c1e044c29608867562089f516644950ce0ea04a7
MinimalistPeach <stremler.lacika77@gmail.com>
2026-01-25 13:10:32 +0100

    Making the base of the invoice-creator page

--- src/app/components/invoice-creator/invoice-creator.ts
+++ src/app/components/invoice-creator/invoice-creator.ts
@@ -1,11 +1,13 @@
+
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-invoice-creator',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './invoice-creator.html',
   styleUrl: './invoice-creator.scss',
 })
-export class InvoiceCreator {
-
-}
+export class InvoiceCreator {}
